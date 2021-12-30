74348e2d7f72b0026630e878aae9c55119317ac2
Kakashi <nickperel@live.com>
2021-12-29 23:00:50 -0500

    finished writing all tests for Employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,11 +9,35 @@ test('set a name', () => {
 test('set an id', () => {
     const newEmployee = new Employee('John', 1, "john@mail.com");
 
-    expect(newEmployee.id).toBe("1")
+    expect(newEmployee.id).toBe(1)
+})
+
+test('set an email', () => {
+    const newEmployee = new Employee('John', 1, "john@mail.com");
+
+    expect(newEmployee.email).toBe("john@mail.com")
 })
 
 test('test for the getName Method', () => {
     const newEmployee = new Employee('John', 1, "john@mail.com");
 
     expect(newEmployee.getName()).toBe("John")
+})
+
+test('test for the getId Method', () => {
+    const newEmployee = new Employee('John', 1, "john@mail.com");
+
+    expect(newEmployee.getId()).toBe(1)
+})
+
+test('test for the getEmail Method', () => {
+    const newEmployee = new Employee('John', 1, "john@mail.com");
+
+    expect(newEmployee.getEmail()).toBe("john@mail.com")
+})
+
+test('test for the getRole Method', () => {
+    const newEmployee = new Employee('John', 1, "john@mail.com");
+
+    expect(newEmployee.getRole()).toBe("employee")
 })
